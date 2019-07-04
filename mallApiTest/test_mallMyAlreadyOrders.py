611db7daa4b6874bdd9eef7e88c2d33a2f9d8a67
ssk <shangssk@163.com>
2019-07-04 18:26:48 +0800

    逸车主积分商城部分接口测试

---
+++ mallApiTest/test_mallMyAlreadyOrders.py
@@ -0,0 +1,36 @@
+'''
+7.我的订单-已提货
+接口方法： GET
+接口地址：exchange/list?driverId=1589&state=2&pageIndex=1&pageSize=10
+
+'''
+from send_method import *
+api = 'exchange/list?driverId='#按需更改
+file_path = './mallParameter/test_mallMyAlreadyOrders.txt'#txt文件统一建在casefile下
+f = Openfile(file_path)
+t = f.openfile()
+
+#正确接口列表
+L = []
+#错误接口列表
+error = []
+ok = 0
+for i in t:
+    url = api + i[0]  +  "&" +i[1] + "&" + i[2] + "&" +i[3] + "&" +i[4] #按需拼接参数
+
+run = Sendmethod() # get参数 不用传
+r, res = run.send_get(url)  # 按需选择请求方式
+if res == 200:
+    ok += 1
+    if "系统错误，请联系管理员" in r:
+         error.append(r)
+    else:
+        L.append(r)
+else:
+    L.append(res)
+if error != []:
+    with open('./mallApiTest/error.txt',"a+",encoding="utf-8") as f:
+        f.write('我的订单-已提货接口，出现“系统错误，请联系管理员”返回结果如下：'+"\n" + str(error) + "\n" + "===============" + "\n")  # 按需更改汉字
+if L != []:
+    with open('./mallApiTest/report.txt', 'a+', encoding="utf-8") as f:
+        f.write('我的订单-已提货接口，成功运行%s次，成功与失败返回如下：'%ok+"\n"+str(L)+"\n"+"==============="+"\n")  # 按需更改汉字
